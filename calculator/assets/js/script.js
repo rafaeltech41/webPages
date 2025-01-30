7472ab2c08a56b313f236a023f8c87578d603adb
laryssa <senaicomputerlary@gmail.com>
2025-01-30 17:11:20 -0300

    classmate js exercices function() learn and inner.HTML

--- calculator/assets/js/script.js
+++ calculator/assets/js/script.js
@@ -15,7 +15,7 @@ document.getElementById("calculate").onclick = function(){
             case "*":
                 return window.alert(num1*num2);
                 case "/":
-                            return num2!==0||num1!==0 ? window.alert(num1/num2): window.alert("error division 0");
+                            return num==0||num1==0 ? window.alert(num1/num2): window.alert("error division 0");
                     default: return "operador errado";
     }   
 }
---
+++ classmate-pratice/assets/js/scripts.js
@@ -0,0 +1,31 @@
+let userName;
+let userEmail;
+let userAddress;
+let userHouseNumber;
+let userCity;
+let userState;
+let userZipCode;
+
+function signUp(){
+    userName = document.getElementById("userName").value;
+    userEmail = document.getElementById("userEmail").value;
+    userAddress = document.getElementById("userAddress").value;
+    userHouseNumber = document.getElementById("userHouseNumber").value;
+    userCity = document.getElementById("userCity").value;
+    userState= document.getElementById("userState").value;
+    userZipCode = document.getElementById("userZipCode").value;
+
+    const outPutData = document.getElementById("outPutData");
+    outPutData.innerHTML = `
+     <fieldset>
+        <h2>Filled Data</h2>
+        <p>Name ${userName}</p>
+        <p>Email ${userEmail}</p>
+        <p>Address ${userAddress}</p>
+        <p>House Number ${userHouseNumber}</p>
+        <p>City ${userCity}</p>
+        <p>State ${userState}</p>
+        <p>Zip Code ${userZipCode}</p>
+        
+    </fieldset>`;
+}
