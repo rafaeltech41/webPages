38f6260acc2dd0648e681af348d3e4d8232987d7
rafaeltech41 <rafaelmoreiralopes41@gmail.com>
2025-01-29 21:31:29 -0300

    PI CONSTANTS calculate circunference

---
+++ calculator/assets/js/script2.js
@@ -0,0 +1,8 @@
+const PI = 3.14159
+let radius;
+let circunference;
+document.getElementById("myButton").onclick =function(){
+    radius = Number(document.getElementById("myText").value);
+    return window.alert(circunference =2*PI*radius);
+}
+
